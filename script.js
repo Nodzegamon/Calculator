c22ce52449e4cbf9475358782946f6868af3319a
Nodzegamon <45968258+Nodzegamon@users.noreply.github.com>
2019-07-10 14:48:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var vue = new Vue({
     el: '#app', // присвоили элемент app
     data: {
-      result: '',
+      result: '',// строка, которая хранится в поле ввода
       numbers: [1,2,3,4,5,6,7,8,9,0], //создаем массив с цифрами
-      operations: ['+','-','*','/'],
+      operations: ['+','-','*','/'],//создаем массив с основными матем. операциями 
     },
-    methods: {
-      input: function(char) {
+    methods: { // создаем обьект методы
+      input: function(char) { // для того, чтобы вводить наши в текстовое поле с помощью нажатия кнопки
         this.result = this.result.toString();
         this.result+=char;
       },
-      reset: function() {
+      reset: function() { // фукция для очищения поля ввода
         this.result = '';
       },
-      calc: function() {
+      calc: function() { // функиция для подсчета результата
         this.result = eval(this.result);
       }
     }
